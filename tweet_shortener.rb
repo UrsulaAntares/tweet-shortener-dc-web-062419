da27e85f6e2430408eadf07d8e37b43cb762b4a1
Ursula Miller <ursula.miller@gmail.com>
2019-06-17 21:53:20 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -21,6 +21,9 @@ def word_substituter(string)
     puts "#{long_word} will become #{short_word}"
     string_array = string.split(" ")
     puts string_array
+    if string_array.include(long_word)
+      puts "We found #{long_word}"
+    end
     string = string_array.join(" ")
     puts string
   end
